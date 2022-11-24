0e13761054517f6bf712e89416618a7efbdcbb24
MohammadHossein Labafbashi <92016155+Labafbashi@users.noreply.github.com>
2022-11-24 16:26:54 +0100

    Add menu

---
+++ Garage/Menu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    internal class Menu
+    {
+        private string title;
+        private string name;
+        private int level;
+
+        private List<string> items = null;
+
+        public Menu(string title, string name, int level)
+        {
+            Title = title;
+            Name = name;
+            Level = level;
+            items = new List<string>();
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Name { get => name; set => name = value; }
+        public int Level { get=> level; set => level = value; }
+
+     
+        public void AddItemMenu(string itemName)
+        {
+            if ((itemName.ToUpper() != "EXIT") || (itemName.ToUpper() != "QUIT"))
+            {
+                items.Add(itemName);
+            }
+        }
+
+        public int DrawMenu() 
+        {
+            int counter = 1;
+            Console.Clear();
+            Console.WriteLine("#####################################################");
+            Console.WriteLine("\t \t "+title);
+            Console.WriteLine("\n "+name+" \n");
+            foreach(string menuItem in items)
+            {
+                Console.WriteLine(counter.ToString()+". " + menuItem);
+                counter++;
+            }
+            if (Level == 0)
+            {
+                Console.WriteLine("0. Exit");
+            }
+            else
+            {
+                Console.WriteLine("0. Back");
+            }
+            
+            Console.WriteLine("\n #####################################################\n");
+            Console.WriteLine("Please select a number: ");
+            try 
+            { 
+                return Int16.Parse(Console.ReadLine()); 
+            }
+            catch (Exception e) 
+            { 
+                Console.WriteLine("Invalid number, Please select a number from the menu.");
+                Console.WriteLine("System Error: \n"+e.ToString());
+                Console.ReadKey(); 
+                return -1; 
+            }
+        }
+    }
+}
--- Garage/Program.cs
+++ Garage/Program.cs
@@ -1,35 +1,114 @@
-﻿internal class Program
+﻿using Garage;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        
+        Menu mn = new Menu("My Garage", "Main Menu",0);
+        
+        mn.AddItemMenu("Reports");
+        mn.AddItemMenu("Activity");
+
+        Menu reportSMN = new Menu("My Garage", "Reports",1);
+        reportSMN.AddItemMenu("List All");
+        reportSMN.AddItemMenu("List Vehicle Type");
+        reportSMN.AddItemMenu("List Vehicle");
+        reportSMN.AddItemMenu("Find / Search");
+
+        Menu activitySMN = new Menu("My Garage", "Activity", 1);
+        activitySMN.AddItemMenu("Vehicle Type");
+        activitySMN.AddItemMenu("Vehicle");
+        activitySMN.AddItemMenu("Garage Capacity");
+
+        Menu dataSMN = new Menu("My Garage", activitySMN.Name, 2);
+        dataSMN.AddItemMenu("Add");
+        dataSMN.AddItemMenu("Edit");
+        dataSMN.AddItemMenu("remove");
+
+        Menu findSMN = new Menu("My Garage", "Find", 2);
+        findSMN.AddItemMenu("Search by Licens Plate");
+        findSMN.AddItemMenu("Search by Owner");
+        findSMN.AddItemMenu("Search by Type");
+        findSMN.AddItemMenu("Search by Color");
+        findSMN.AddItemMenu("CustomSearch");
+
+
         bool exit = true;
         do
         {
-            switch (DrawMainMenu()) {
+            bool subLevel1Exit = true;
+            switch (mn.DrawMenu()) {
                 case 0:
                     exit= false;
+                    break;
+                case 1: //##################################################### Report Sub Menu
+                    do
+                    {
+                        switch (reportSMN.DrawMenu())
+                        {
+                            case 0: //Back to previous menu
+                                subLevel1Exit= false;
+                                break;
+                            case 1: //List All
+                                
+                                break; 
+                            case 2: // List Vehicle Type
+                                break;
+                            case 3: // List Vehicle
+                                break;
+                            case 4: // Find
+                                bool subLevel2Exit = true;
+                                do
+                                {
+                                    switch (findSMN.DrawMenu())
+                                    {
+                                        case 0:
+                                            subLevel2Exit = false;
+                                            break;
+                                        case 1: //Search by Licens Plate
+                                            break;
+                                        case 2: //Search by Owner
+                                            break;
+                                        case 3: //Search by Type
+                                            break;
+                                        case 4: //Search by Color
+                                            break;
+                                        case 5: //CustomSearch
+                                            break;
+                                        default:
+                                            break;
+                                    }
+                                } while (subLevel2Exit);
+                                break;
+                            default:
+                                break;
+                        }
+                    } while (subLevel1Exit);
+                        
+                    break;
+                case 2: //##################################################### Activity Sub Menu
+                    do
+                    {
+                        switch (activitySMN.DrawMenu())
+                        {
+                            case 0:
+                                subLevel1Exit = false; break;
+                            case 1: //Vehicle Type
+
+                                break;
+                            case 2: //Vehicle
+                                break;
+                            case 3: //Garage Capacity
+                                break;
+                            default:
+                                break;
+                        }
+                    }while(subLevel1Exit);
                     break;
                 default:
                     break;  
             }
-                
-                
         }while(exit);
-    }
-
-    private static int DrawMainMenu()
-    {
-        Console.Clear();
-        Console.WriteLine("#####################################################");
-        Console.WriteLine("\t \t My Garage");
-        Console.WriteLine("\n Main Menu\n");
-        Console.WriteLine("1. Show Garage");
-        Console.WriteLine("2. Reports");
-        Console.WriteLine("3. Activity");
-        Console.WriteLine("0. Exit");
-        Console.WriteLine("Please select a number: ");
-        try { return (Int16.Parse(Console.ReadLine())); }
-        catch (Exception e){ Console.WriteLine("Invalid number, Please select a number from the menu."); Console.ReadKey(); return 100; }
-        
     }
 }
